4ad4bce8aab565ea9df8d1dcc60862f8bcf66637
manojreddy56 <124623327+manojreddy56@users.noreply.github.com>
2024-06-03 00:00:47 +0530

    Add files via upload

--- Linkedlist.py
+++ Linkedlist.py
@@ -27,12 +27,45 @@ class Linkedlist:
         self._tail = newest
         self._size += 1
         
+    def Add_first(self, e):
+        newest = _Node(e, None)
+        if self.isempty():
+            self._head = newest
+            self._tail = newest
+        else:
+            newest._next = self._head
+            self._head = newest
+        self._size += 1
+        
+    def Add_any(self, e, position):
+        newest = _Node(e, None)
+        p = self._head
+        i = 1
+        while i < position-1:
+            p = p._next
+            i += 1
+        newest._next = p._next
+        p._next = newest
+        self._size += 1
+        
     def display(self):
         p = self._head
         while p:
             print(p._element, end="  ")
             p = p._next
         print()
+    
+    def search(self, key):
+        p = self._head
+        index = 0
+        while p:
+            if p._element == key:
+                return index
+            else:
+                p = p._next
+                index += 1
+        else:
+            return "Element not found"
             
             
 L = Linkedlist()
@@ -44,3 +77,9 @@ print("size: ", len(L))
 L.Add_last(8)
 L.Add_last(3)
 print("size: ", len(L))
+print(L.search(8))
+print(L.search(10))
+L.Add_first(6)
+L.display()
+L.Add_any(23, 3)
+L.display()
